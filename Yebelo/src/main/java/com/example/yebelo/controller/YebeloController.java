b7bd748375b4bfcad5edf2f485d129dcb721061b
ss <ss@DESKTOP-3502CHE>
2021-07-13 18:30:57 +0530

    dao controller and service Classes created

--- Yebelo/src/main/java/com/example/yebelo/controller/YebeloController.java
+++ Yebelo/src/main/java/com/example/yebelo/controller/YebeloController.java
@@ -1,5 +1,19 @@
 package com.example.yebelo.controller;
 
-public class yebelocontroller {
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
 
+import com.example.yebelo.entity.EntityClass;
+
+@RestController
+@RequestMapping
+public class YebeloController {
+	
+	@GetMapping("/FetchNextNumber")
+	public EntityClass getNumbers()
+	{
+		return null;
+		
+	}
 }
